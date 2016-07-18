d48c6eafdc486d6127b67b32fa6ffe36538828d4
Maria300196 <000_mari@mail.ru>
2016-07-18 13:06:44 +0300

    Initializer list repaire

---
+++ int.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Foo
+{
+	int count;
+	vector<int> vec;
+
+public:
+	Foo(int val): count(val * 3), vec(count)
+{
+}
+int vecSize(){return vec.size();}
+};
+
+int main()
+{
+	Foo f(5);
+	std::cout<<f.vecSize();
+	return 0;
+}
+
